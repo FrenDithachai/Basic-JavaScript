ac7b123a2772ab8e1d29091e6e28f8ab10f9878b
FrenDithachai <dithachai.fren@gmail.com>
2024-03-28 09:21:43 +0700

    Basic-javaScript

---
+++ 2.การแจ้งเตือนstype.js
@@ -0,0 +1,6 @@
+// document.write("<h1>Fren</h1>");
+// document.write("<p>Frengawdadawad</p>");
+// alert("สวัสดี");
+// console.log("herllo")
+// console.error("เจอไวรัส");
+// console.warn("ข้อผิดพลาด")
