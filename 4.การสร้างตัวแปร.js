04b63ffc46a32a8a78233c3a31548b7817813af6
FrenDithachai <dithachai.fren@gmail.com>
2024-03-28 09:34:59 +0700

    Basic-javaScript

---
+++ 4.การสร้างตัวแปร.js
@@ -0,0 +1,17 @@
+// let nme; //ไม่มีการกำหนดค่า
+// nme = "Fren"
+// let age =20;
+
+let nme = "fren",age=20 ,Nme = "Bam",NME=100;
+
+console.log(nme);
+console.log(NME);
+document.write(nme);
+console.log("อายุ =",age);
+document.write(Nme);
+
+nme ="เฟน"; //การเปลี่ยนค่า
+console.log(nme)
+
+nme ="FOMM"
+console.log(nme)
